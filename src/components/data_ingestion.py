464ad0cb70256633e99aac8fdf32025faf745a49
rocking kamal <vayalpatikamalthej@gmail.com>
2023-04-22 16:56:38 +0530

    Created artifatcs

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -22,7 +22,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df=pd.read_csv('/Users/kamalthej/ML_project/notebook/data/stud.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
